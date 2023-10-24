30e938003c928fe36c5826842046c5f1bee64a11
kareem-n <kareemnasser1369@gmail.com>
2023-10-24 22:35:32 +0200

    form page hands on design

---
+++ src/components/login/Form.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+
+function LoginForm() {
+
+
+  const handleFocus = (e) => {
+    e.target.parentElement.classList.add("theme-border-color");
+    e.target.nextElementSibling.classList.add("text-xs", "top-1/3");
+
+  }
+  const handleBlur = (e) => {
+    e.target.parentElement.classList.remove("theme-border-color");
+
+    e.target.nextElementSibling.classList.remove("text-xs", "top-1/3");
+
+  }
+
+  return (
+    <div className="px-10 flex items-center flex-col mt-7">
+
+      <div className="w-full bg-white relative rounded-lg shadow-lg border">
+        <input
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          type="text"
+          className="w-full bg-transparent pt-6 relative z-10 py-2 px-5  outline-none"
+        />
+        <label
+          className="absolute text-gray-400 left-5 top-1/2 transition-all -translate-y-1/2" htmlFor="">
+          Email
+        </label>
+      </div>
+      <div className="w-full mt-4 bg-white relative rounded-lg shadow-lg border">
+        <input
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          type="text"
+          className="w-full bg-transparent pt-6 relative z-10 py-2 px-5  outline-none"
+        />
+        <label
+          className="absolute text-gray-400 left-5 top-1/2 transition-all -translate-y-1/2" htmlFor="">
+          Password
+        </label>
+      </div>
+      <div className="w-full flex items-center mt-3">
+        <input id="rem" type="checkbox" className="mr-2 mt-1" />
+        <label htmlFor="rem">
+          remember me
+        </label>
+      </div>
+
+      <button className="mt-3 theme-bg-color w-full py-2 text-white opacity-80 hover:opacity-100">
+        Login
+      </button>
+
+      <div className="w-full mt-3 text-sm text-center">
+        Create an account? <Link className="text-blue-500">Register...</Link> 
+      </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default LoginForm
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,29 @@
-
+import LoginForm from './Form';
+import side from './side.jpg';
 function Login() {
   return (
     <div className="min-h-screen flex items-center justify-center ">
-        <div className="bg-red-400 px-5 py-3 rounded-md">
-            kkk
-        </div>
 
+      <div className="container px-10 grid grid-cols-6 mt-10">
+        <div className="col-span-2 col-start-2 flex items-center justify-center rounded-tl-lg rounded-bl-lg bg-neutral-100 p-10">
+          <div className=" w-full">
+            <h1 className='text-center text-4xl'>Welcome again
+              <br />
+              <span className='text-xl'>
+                Ready for <span className='theme-text-color'>shopping!
+                </span>
+              </span>
+
+
+            </h1>
+
+            <LoginForm />
+          </div>
+
+        </div>
+        <img src={side} className='col-span-2 rounded-tr-lg rounded-br-lg' alt="" />
+
+      </div>
 
     </div>
   )
